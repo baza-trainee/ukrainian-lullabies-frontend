30e911b5568b03d497bd0837f65f7cf85b297838
shologin <mockingbird@mail.com>
2023-09-14 01:42:12 +0300

    feat: add Logo component; created folder SVGComponents and added Logo as component so now it's reusable

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import "./Header.css";
+import { Link } from "react-router-dom";
+import { Logo } from "../SVGComponents/Logo";
 
 export const Header = () => {
-  return <>
-    <h1>Header</h1>
-    <Link to="/about" > About
-    </Link>
-  </>;
+  return (
+    <div className="header container">
+      <Logo width="56" height="53" />
+      <h1>Header</h1>
+      <Link to="/about"> About</Link>
+    </div>
+  );
 };
