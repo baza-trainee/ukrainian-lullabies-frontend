15bcf6c55139b2068b30b96b87ca31198142d1d5
Diana Tuz <dianatuz12@gmail.com>
2023-09-15 17:43:50 +0300

    page 404

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return <>
-    <h1>Header</h1>
+    <h1>Header   </h1>
     <Link to="/about" > About
     </Link>
   </>;
