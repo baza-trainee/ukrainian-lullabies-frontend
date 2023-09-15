808785febb1ad0d89dc9b03b93f70117f96c4970
Diana Tuz <124447185+diana-tuz@users.noreply.github.com>
2023-09-15 17:46:41 +0300

    Feature/404page (#18)
    * initial commit
    
    * modified:   prettier.js
    
    * create error page
    
    * create error page
    
    * page 404
    
    ---------
    
    Co-authored-by: Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 import { Outlet } from "react-router-dom";
-import { Header } from "../components/Header/Header";
-import { Footer } from "../components/Footer/Footer";
+import {Header} from "../components/Header/Header";
+import {Footer} from "../components/Footer/Footer";
 import "./Layouts.css";
 
 export default function RootLayout() {
