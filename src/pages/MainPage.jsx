673b2a67a922cade7d20914b45f98191f71702c3
IrynaShashko <95182459+IrynaShashko@users.noreply.github.com>
2023-09-15 11:38:58 +0300

    fix: add changes in file

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import { MapTabs } from "../components/MapTabs/MapTabs";
 
 export const MainPage = () => {
   return (
     <main>
       <p>Main Page</p>
-      
+      <MapTabs />
+      <Outlet />
     </main>
   )
 };
