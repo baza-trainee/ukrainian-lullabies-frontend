563c68a5612fdb54a20afd5d5c4e60664920e5b8
Diana Tuz <124447185+diana-tuz@users.noreply.github.com>
2023-09-15 16:37:45 +0300

    Feature/404page (#16)
    * initial commit
    
    * modified:   prettier.js
    
    * create error page
    
    * create error page

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React from 'react';
 import { Outlet } from "react-router-dom";
-import { Header } from "../components/Header/Header";
-import { Footer } from "../components/Footer/Footer";
+import {Header} from "../components/Header/Header";
+import {Footer} from "../components/Footer/Footer";
 import "./Layouts.css";
 
 export default function RootLayout() {
   return (
-    <div className='background-dark'>
-      <div className='content'>
-        <Header />
-        <main className='container'> 
-          <Outlet />
-        </main>
-        <Footer />
+    <>
+      <div className='bg-dark'>
+      <div className="bg-left">
+        <img src="/src/assets/images/ornamentsDark.png" alt="" />
+      </div>
+      <div>
+        <div className='content bg-dark'>
+          <Header />
+          <main className='container'>
+            <Outlet />
+          </main>
+          <Footer />
+        </div>
+        <div className="bg-right">
+          <img src="/src/assets/images/ornamentsDark.png" alt="" />
+        </div>
       </div>
     </div>
+    </>
   );
 }
