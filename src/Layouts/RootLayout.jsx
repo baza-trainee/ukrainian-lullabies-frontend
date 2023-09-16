78dd718ccbb6e8a5a93e2d26f120f83fe2217924
Diana Tuz <124447185+diana-tuz@users.noreply.github.com>
2023-09-16 11:51:53 +0300

    Fix/immage for bg (#26)
    * initial commit
    
    * modified:   prettier.js
    
    * change bg-image
    
    * change immage
    
    * fir url
    
    * fix/position-fixed
    
    * fix-positio-fixed
    
    * fix-bg-image
    
    * some changes
    
    * fix immage for bg
    
    ---------
    
    Co-authored-by: Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -8,9 +8,9 @@ export default function RootLayout() {
   return (
     <>
 
-      <div className='bg-dark'>
-        <div className="bg-dark-img-left"></div>  
 
+      <div className='bg-dark body'>
+        <div className="bg-dark-img-left"></div>  
         <div className='content bg-dark'>
           <Header />
           <main className='container'>
