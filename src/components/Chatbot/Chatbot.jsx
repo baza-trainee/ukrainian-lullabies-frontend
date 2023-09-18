5f80d2b8143f2353bb9462bd48c815f98981d3a1
BodNS <bondarenokn@gmail.com>
2023-09-18 10:36:55 +0200

    fix: changed Chatbot button

--- src/components/Chatbot/Chatbot.jsx
+++ src/components/Chatbot/Chatbot.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import classNames from "classnames";
 import rules from "./text";
 import "./Chatbot.css";
 
 const Chatbot = () => {
+  const isLightTheme = useSelector((state) => state.theme.isLightTheme);
+
   const chatRules = rules.map((rule, index) => (
     <li key={ index + 1 } className='chat_text'>
       { rule }
@@ -10,20 +15,20 @@ const Chatbot = () => {
   ));
 
   return (
-    <div className="chat_wrapper container">
+    <section className="chat_wrapper container">
       <div className='chat_info'>
         <p className="chat_title">Чат-бот</p>
         <ul className='chat_rule_list'>
           { chatRules }
         </ul>
-        <div className='chat_button'>
-          <a href='#'>Грати</a>
+        <div>
+          <NavLink to="#" className={ classNames('button', 'chat_button', { 'button_light': isLightTheme, }) }> Грати </NavLink>  
         </div>
       </div>
       <div className='chat_picture'>
         <a href='#' className='chat_image'></a>
       </div>
-    </div>
+    </section>
   )
 };
 
