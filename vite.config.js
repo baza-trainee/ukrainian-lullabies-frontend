36904003af0c5e7e94c2a442ec424d4cd276e64a
Diana Tuz <124447185+diana-tuz@users.noreply.github.com>
2023-09-18 22:48:22 +0300

    Fix/vercell problem (#49)
    * initial commit
    
    * modified:   prettier.js
    
    * add animation for mainPage
    
    * add configuration
    
    * add framer-motion
    
    * config
    
    * fix1
    
    * fix2
    
    * fix3
    
    * a
    
    * add alias
    
    ---------
    
    Co-authored-by: Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import VitePluginAlias from 'vite-plugin-alias';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    VitePluginAlias({
+      entries: [
+        { find: 'framer-motion', replacement: 'framer-motion/dist/framer-motion.cjs.js' },
+      ],
+    }),
   ],
-  build: {
-    rollupOptions: {
-      external: ['framer-motion'], // Перевірте, чи тут немає 'framer-motion'
-    },
-  },
-})
+});
+
