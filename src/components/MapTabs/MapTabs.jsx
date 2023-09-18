2bbd5995b8006f08f69e3da8bab0f0f486b8a454
IrynaShashko <95182459+IrynaShashko@users.noreply.github.com>
2023-09-18 08:53:46 +0300

    fix: deleted theme

--- src/components/MapTabs/MapTabs.jsx
+++ src/components/MapTabs/MapTabs.jsx
@@ -1,31 +1,19 @@
 import React from "react";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import { NavLink, Outlet } from "react-router-dom";
-import classNames from "classnames";
+// import classNames from "classnames";
 import tabsSvg from "../../assets/images/ornamentsMapTabsSection.svg"
 import "./map.css"
 
 export const MapTabs = () => {
-    const isLightTheme = useSelector((state) => state.theme.isLightTheme);
+    // const isLightTheme = useSelector((state) => state.theme.isLightTheme);
 
     return (<section className="tabs">
-        <h2 className={classNames({
-            "map-tabs__text map-tabs__light": isLightTheme,
-            "map-tabs__text map-tabs__dark": !isLightTheme,
-        })}>Музей колискової</h2>
+        <h2 className="map-tabs__text">Музей колискової</h2>
         <div className="map-tabs">
-            <NavLink className={classNames("map-tabs__button", "tabs1", {
-                "map-tabs__light": isLightTheme,
-                "map-tabs__dark": !isLightTheme,
-            })} to={"/map"}>Традиційні колискові</NavLink>
-            <NavLink className={classNames("map-tabs__button", "tabs2", {
-                "map-tabs__light": isLightTheme,
-                "map-tabs__dark": !isLightTheme,
-            })} to={"/songs"}>Співаємо разом</NavLink>
-            <NavLink className={classNames("map-tabs__button", "tabs3", {
-                "map-tabs__light": isLightTheme,
-                "map-tabs__dark": !isLightTheme,
-            })} to={"/anima"}>Колискові в анімаціях</NavLink>
+            <NavLink className="map-tabs__button tabs1" to={"/map"}>Традиційні колискові</NavLink>
+            <NavLink className="map-tabs__button tabs2" to={"/songs"}>Співаємо разом</NavLink>
+            <NavLink className="map-tabs__button tabs3" to={"/anima"}>Колискові в анімаціях</NavLink>
         </div>
         <Outlet />
         <img src={tabsSvg} alt="tabsSvg" />
