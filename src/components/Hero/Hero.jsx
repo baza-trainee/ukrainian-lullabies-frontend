31141406a25d29b647140c16d2bb08a1f2153094
Olena <newsroommy@gmail.com>
2023-09-19 18:29:44 +0300

    fix css

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -55,42 +55,6 @@ const Hero = () => {
   };
 
   return (
-import React from "react";
-
-import { motion } from "framer-motion";
-import { useSelector } from "react-redux";
-import { NavLink } from "react-router-dom";
-import classNames from "classnames";
-
-import { getLightTheme } from "../../redux/theme/themeSelectors";
-
-import KolyIcon from "../../icons/KolyIcon";
-import SIcon from "../../icons/SIcon";
-import KovaIcon from "../../icons/KovaIcon";
-import OundIcon from "../../icons/OundIcon";
-import OrnamentsLeftIcon from "../../icons/OrnamentsLeftIcon";
-import OrnamentsRightIcon from "../../icons/OrnamentsRightIcon";
-import ButtonShare from "./Button/ButtonShare";
-
-import "./hero.css";
-
-const Hero = () => {
-  // theme toggle
-  const isLightTheme = useSelector(getLightTheme);
-
-  const animationElement = {
-    hidden: {
-      y: -50,
-      opacity: 0,
-    },
-    visible: (custom) => ({
-      y: 0,
-      opacity: 1,
-      transition: { ease: "easeOut", duration: 2, delay: custom * 0.3 },
-    }),
-  };
-
-  return (
     <motion.div
       initial="hidden"
       whileInView="visible"
@@ -161,7 +125,6 @@ const Hero = () => {
       </div>
     </motion.div>
   );
-};
 };
 
 export default Hero;
