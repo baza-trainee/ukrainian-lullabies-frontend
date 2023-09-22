6611af0390f8f3ad67fed6066fa91ad2871c55ce
shologin <mockingbird@mail.com>
2023-09-22 13:32:42 +0300

    fix: fixes to cookies; - added space between text and underline in cookiesSettings - blocked click outside the modal;

--- src/components/Cookies/Cookies.jsx
+++ src/components/Cookies/Cookies.jsx
@@ -10,13 +10,21 @@ export const Cookies = () => {
     const cookies = document.getElementById("cookies");
     cookies.style.display = "none";
   };
+
+  const closeCookiesBar = () => {
+    const cookies = document.getElementById("cookies");
+    cookies.style.display = "none";
+  };
   return (
     <div className={classNames("cookies", { "cookies-light": isLightTheme })} id="cookies">
+      <button className="cookies-close-button" onClick={closeCookiesBar}>
+        &times;
+      </button>
       <p className="cookies-text text-base-regular">
         Натискаючи «Прийняти всі файли cookie», ви погоджуєтеся на збереження файлів cookie на вашому пристрої для покращення
         навігації сайтом, аналізу використання сайту та допомоги в наших маркетингових зусиллях.
       </p>
-      <CookiesModal isLightTheme={isLightTheme} />
+      <CookiesModal isLightTheme={isLightTheme} closeCookiesBar={closeCookiesBar} />
       <button
         className={classNames("cookies-accept-button", "text-base-semibold", { "cookies-accept-button-light": isLightTheme })}
         onClick={acceptAllClick}
