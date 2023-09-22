b96dc979b1a1911a6ecad6651bae5f5b79495e31
shologin <136815194+shologin@users.noreply.github.com>
2023-09-22 20:51:11 +0300

    fix: scroll to top; (#69)
    - fixed scroll to top on click on Logo in footer
    
    Co-authored-by: shologin <mockingbird@mail.com>

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -72,10 +72,8 @@ export const Footer = () => {
       </button>
       <div className="footer-wrapper container text-sm">
         <div className="separation-line"></div>
-        <div className="footer-logo">
-          <Link to="/" onClick={scrollToTop}>
-            {isLightTheme ? <LogoLight width="92" height="88" /> : <LogoDark width="92" height="88" />}
-          </Link>
+        <div className="footer-logo" onClick={scrollToTop}>
+          <Link to="/">{isLightTheme ? <LogoLight width="92" height="88" /> : <LogoDark width="92" height="88" />}</Link>
         </div>
         <ul className="footer-docs-wrapper">
           <li>
