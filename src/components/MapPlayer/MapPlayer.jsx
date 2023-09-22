b66afdf03975ec355a4a5731fb4e6cd30f23fa86
IrynaShashko <95182459+IrynaShashko@users.noreply.github.com>
2023-09-22 15:07:20 +0300

    feat: add button

--- src/components/MapPlayer/MapPlayer.jsx
+++ src/components/MapPlayer/MapPlayer.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export const MapPlayer = () => {
-  return <div style={{ height: "600px", display: "flex", justifyContent: "center", alignItems: "center" }}>
+  return <div style={{ height: "600px", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
     <h2>Map player</h2>
+    <NavLink className="button" style={{ marginTop: "150px" }} to={"/"}>На головну</NavLink>
   </div>;
 };
