6f664186eeb6074898aa27f93820292b30aa827f
Diana Tuz <124447185+diana-tuz@users.noreply.github.com>
2023-09-23 15:11:01 +0300

    Bugfixing/animation (#80)
    * initial commit
    
    * modified:   prettier.js
    
    * 123
    
    * add general styles for mobile
    
    * fix animation
    
    ---------
    
    Co-authored-by: Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>

--- src/components/GeneralTitle/GeneralTitle.jsx
+++ src/components/GeneralTitle/GeneralTitle.jsx
@@ -1,41 +1,26 @@
 import React from "react";
-import { motion } from "framer-motion";
 import SIcon from "../../icons/SIcon";
 
 export const GeneralTitle = () => {
-  const animationElement = {
-    hidden: {
-      y: -50,
-      opacity: 0,
-    },
-    visible: (custom) => ({
-      y: 0,
-      opacity: 1,
-      transition: { ease: "easeOut", duration: 2, delay: custom * 0.3 },
-    }),
-  };
+
+
   return (
-    <motion.div
-      initial="hidden"
-      whileInView="visible"
-      className="kolyskovaWrap margin-bottom">
-      <motion.div
-        custom={ 1 }
-        variants={ animationElement }
-        className="kolyskovaWrap"
-      >
-        <div className="kolyIcon text-5xl">
-          Koly
-        </div>
-        <div className="letterS">
-          <SIcon />
-        </div>
-        <div className="kovaIcon text-5xl">
-          Kova
-        </div>
-        <div className="oundIconWrap text-5xl">
-          ound
-        </div>
-      </motion.div>
-    </motion.div>)
+
+    <div
+      className="kolyskovaWrap"
+    >
+      <div className="kolyIcon text-5xl">
+        Koly
+      </div>
+      <div className="letterS">
+        <SIcon />
+      </div>
+      <div className="kovaIcon text-5xl">
+        Kova
+      </div>
+      <div className="oundIconWrap text-5xl">
+        ound
+      </div>
+    </div>
+  )
 }
