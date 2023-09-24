a727525a78cf17eb5654f6395ec2389c5387e46d
Olena <newsroommy@gmail.com>
2023-09-24 12:21:12 +0300

    fix: css pop-up

--- src/components/FormFeedBack/PopUpFeedBack/PopUpFeedBack.jsx
+++ src/components/FormFeedBack/PopUpFeedBack/PopUpFeedBack.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import OrnamentsLeftIcon from "../../../icons/OrnamentsLeftIcon";
+import OrnamentsRightIcon from "../../../icons/OrnamentsRightIcon";
 
 import "./pop-up-feedback.css";
 
 const PopUpFeedBack = () => {
   return (
     <div className="popup-message">
-      <p>Дякуємо, Ваше повідомлення відправлене!</p>
+      <OrnamentsLeftIcon />
+      <p className="text-2xl pop-up-text-form">
+        Дякуємо, Ваше повідомлення відправлене!
+      </p>
+      <OrnamentsRightIcon />
     </div>
   );
 };
