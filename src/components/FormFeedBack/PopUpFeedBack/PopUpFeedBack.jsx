82d6b600510183d4719c5076532df1ac1f3d72a4
Olena <newsroommy@gmail.com>
2023-09-24 13:41:24 +0300

    fix: css pop-up

--- src/components/FormFeedBack/PopUpFeedBack/PopUpFeedBack.jsx
+++ src/components/FormFeedBack/PopUpFeedBack/PopUpFeedBack.jsx
@@ -1,15 +1,29 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import classNames from "classnames";
+
+import { getLightTheme } from "../../../redux/theme/themeSelectors";
+
 import OrnamentsLeftIcon from "../../../icons/OrnamentsLeftIcon";
 import OrnamentsRightIcon from "../../../icons/OrnamentsRightIcon";
 
 import "./pop-up-feedback.css";
 
 const PopUpFeedBack = () => {
+  // theme toggle
+  const isLightTheme = useSelector(getLightTheme);
+
   return (
-    <div className="popup-message">
+    <div
+      className={classNames("popup-message", {
+        "popup-message-dark": !isLightTheme,
+        "popup-message-white": isLightTheme,
+      })}
+      // className="popup-message"
+    >
       <OrnamentsLeftIcon />
       <p className="text-2xl pop-up-text-form">
-        Дякуємо, Ваше повідомлення відправлене!
+        Дякуємо, <br /> Ваше повідомлення відправлене!
       </p>
       <OrnamentsRightIcon />
     </div>
