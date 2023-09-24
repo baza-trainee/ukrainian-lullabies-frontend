39c279b49a8f71cfe997da4094cc44627c32251e
Olena <newsroommy@gmail.com>
2023-09-24 13:41:24 +0300

    fix: css pop-up

--- src/components/FormFeedBack/FormFeedBack.jsx
+++ src/components/FormFeedBack/FormFeedBack.jsx
@@ -75,10 +75,10 @@ const FormFeedBack = () => {
 
   const handleShowPopUp = () => {
     if (formikRef.current.isValid && formikRef.current.dirty) {
-      setShowSuccessMessage(true);
-      setTimeout(() => {
-        setShowSuccessMessage(false);
-      }, 3000);
+      // setShowSuccessMessage(true);
+      // setTimeout(() => {
+      //   setShowSuccessMessage(false);
+      // }, 3000);
     }
   };
 
