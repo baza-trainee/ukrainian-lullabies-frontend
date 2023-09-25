f68d12f4506f2bb9ca2fbb9687b1c512896c9b56
Olena <newsroommy@gmail.com>
2023-09-25 17:19:20 +0300

    add en for placeholder and fix css in popUpFeedBack

--- src/components/FormFeedBack/FormFeedBack.jsx
+++ src/components/FormFeedBack/FormFeedBack.jsx
@@ -74,9 +74,9 @@ const FormFeedBack = () => {
   const handleShowPopUp = () => {
     if (formikRef.current.isValid && formikRef.current.dirty) {
       setShowSuccessMessage(true);
-      // setTimeout(() => {
-      //   setShowSuccessMessage(false);
-      // }, 3000);
+      setTimeout(() => {
+        setShowSuccessMessage(false);
+      }, 3000);
     }
   };
 
