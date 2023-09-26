49352f9a910817cb83f728250bcee18152a64c49
Diana Tuz <124447185+diana-tuz@users.noreply.github.com>
2023-09-26 23:11:31 +0300

    add prototype (#108)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import PopularSongsSlice from "./PopularSongs/PopularSongsSlice";
 import themeReducer from "./theme/themeSlice";
+import dataReducer from "./DataSlice";
 
 
 const store = configureStore({
   reducer: {
     theme: themeReducer,
     popularSongs: PopularSongsSlice,
+     data: dataReducer,
   },
 });
 
