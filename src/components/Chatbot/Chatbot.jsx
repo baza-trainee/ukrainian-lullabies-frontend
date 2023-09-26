e5d3922c9eabac3beb0c8c4d6b8d1c75589fccc7
BodNS <71136538+BodNS@users.noreply.github.com>
2023-09-26 13:57:55 +0200

    Chatbot mobile (#102)
    * commit packege-lock.json
    
    * changed text in component AboutUs
    
    * feat: AboutUsInfo mobile version
    
    * feat: Chatbot mobile witout shrift
    
    * fix: AboutUs mobile

--- src/components/Chatbot/Chatbot.jsx
+++ src/components/Chatbot/Chatbot.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
 import classNames from "classnames";
 import "./Chatbot.css";
@@ -10,11 +10,16 @@ const Chatbot = () => {
   const isLightTheme = useSelector((state) => state.theme.isLightTheme);
   const { t } = useTranslation();
 
-  const rules = [t('playEducationalGameWithChatbot'), t('youCanGuessWhichRegion'), t('youCanLearnAboutThePlots'), t('letsPlay')];
+  const rules = [
+    t("playEducationalGameWithChatbot"),
+    t("youCanGuessWhichRegion"),
+    t("youCanLearnAboutThePlots"),
+    t("letsPlay"),
+  ];
 
   const chatRules = rules.map((rule, index) => (
-    <li key={ index + 1 } className='text-base chat_text'>
-      { rule }
+    <li key={index + 1} className="text-base chat_text">
+      {rule}
     </li>
   ));
 
@@ -23,49 +28,50 @@ const Chatbot = () => {
       y: -50,
       opacity: 0,
     },
-    visible: custom => ({
+    visible: (custom) => ({
       y: 0,
       opacity: 1,
       transition: { ease: "easeOut", duration: 2, delay: custom * 0.3 },
     }),
-  }
+  };
 
   return (
     <motion.div
       initial="hidden"
       whileInView="visible"
-
-      className="container chat_wrapper">
-      <div className='chat_info'>
-        <motion.p
-          custom={ 1 }
-          variants={ animationElement }
-          className="chat_title">
-          { t('chatBot') }
-
+      className="container chat_wrapper"
+    >
+      <div className="chat_info">
+        <motion.p custom={1} variants={animationElement} className="text-3xl">
+          {t("chatBot")}
         </motion.p>
         <motion.ul
-          custom={ 2 }
-          variants={ animationElement }
-          className='chat_rule_list'>
-          { chatRules }
+          custom={2}
+          variants={animationElement}
+          className="chat_rule_list"
+        >
+          {chatRules}
         </motion.ul>
-        <motion.div
-          custom={ 3 }
-          variants={ animationElement }
-          className='button'>
-          <Link href='#' className='text-base-semibold'>{ t('play') }</Link>
-
-        </motion.div>
       </div>
+      <motion.div custom={3} variants={animationElement} className="button chat_button">
+        <Link href="#" className="text-base-semibold">
+          {t("play")}
+        </Link>
+      </motion.div>
       <motion.div
-        custom={ 3 }
-        variants={ animationElement }
-        className='chat_picture'>
-        <Link href='#' className={ classNames("chat_image_dark", { "chat_image_white": isLightTheme }) }></Link>
+        custom={3}
+        variants={animationElement}
+        className="chat_picture"
+      >
+        <Link
+          href="#"
+          className={classNames("chat_image_dark", {
+            chat_image_white: isLightTheme,
+          })}
+        ></Link>
       </motion.div>
     </motion.div>
-  )
+  );
 };
 
 export default Chatbot;
