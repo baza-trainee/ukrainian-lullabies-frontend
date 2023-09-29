83016d29999ac80b68a5ca11680df71a402db1c5
Olena Bondarenko <107562787+OllBond@users.noreply.github.com>
2023-09-29 17:27:32 +0300

    Feature/fix form fedd back (#116)
    * add feedBackForm
    
    * add pop-up
    
    * fix: css pop-up
    
    * fix: css pop-up
    
    * add setTimeout
    
    * add en language
    
    * add en for placeholder and fix css in popUpFeedBack
    
    * fix pop-up in form add overlay
    
    * fix fields name, email, underTextArea, fix css in hero margin-top 26px
    
    * fix: language en and uk for schema
    
    * fix: delete comments in translations js
    
    * add adaptation
    
    * fix: css pop-up
    
    * fix: css pop-up
    
    * add setTimeout
    
    * add en language
    
    * add en for placeholder and fix css in popUpFeedBack
    
    * fix pop-up in form add overlay
    
    * fix fields name, email, underTextArea, fix css in hero margin-top 26px
    
    * fix:padding in hero

--- src/components/FormFeedBack/PopUpFeedBack/PopUpFeedBack.jsx
+++ src/components/FormFeedBack/PopUpFeedBack/PopUpFeedBack.jsx
@@ -1,32 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { useSelector } from "react-redux";
-import classNames from "classnames";
 
-import { getLightTheme } from "../../../redux/theme/themeSelectors";
+import classNames from "classnames";
 
 import OrnamentsLeftIcon from "../../../icons/OrnamentsLeftIcon";
 import OrnamentsRightIcon from "../../../icons/OrnamentsRightIcon";
 
 import "./pop-up-feedback.css";
 
-const PopUpFeedBack = ({ popUpThank, popupText }) => {
-  // theme toggle
-  const isLightTheme = useSelector(getLightTheme);
+const PopUpFeedBack = ({ popUpThank, popupText, isLightTheme }) => {
+  const overlayClasses = classNames("overlay", {
+    "overlay-light": isLightTheme,
+  });
+
+  const popupClasses = classNames("popup-message", {
+    "popup-message-dark": !isLightTheme,
+    "popup-message-white": isLightTheme,
+  });
 
   return (
-    <div
-      className={classNames("popup-message", {
-        "popup-message-dark": !isLightTheme,
-        "popup-message-white": isLightTheme,
-      })}
-    >
-      <OrnamentsLeftIcon />
-      <div className="text-2xl pop-up-text-form">
-        <p>{popUpThank}</p>
-        <p>{popupText}</p>
+    <div className={overlayClasses}>
+      <div className={popupClasses}>
+        <OrnamentsLeftIcon />
+        <div className="text-2xl pop-up-text-form">
+          <p>{popUpThank}</p>
+          <p>{popupText}</p>
+        </div>
+        <OrnamentsRightIcon />
       </div>
-      <OrnamentsRightIcon />
     </div>
   );
 };
@@ -35,4 +36,5 @@ export default PopUpFeedBack;
 PopUpFeedBack.propTypes = {
   popUpThank: PropTypes.string.isRequired,
   popupText: PropTypes.string.isRequired,
+  isLightTheme: PropTypes.func,
 };
---
+++ src/components/FormFeedBack/translations.js
@@ -0,0 +1,35 @@
+const translations = {
+  en: {
+    schema: {
+      nameInvalidName: "Please enter a valid name",
+      nameNotAllowedMessage:
+        'It is forbidden to use the following symbols:«% ^ * | ~ {} ; "<>. , /»',
+      nameMinLengthMessage: "The number of characters must be at least 2",
+      nameMaxLengthMessage: "The number of characters must not exceed 30",
+      requiredMessage: "This field is mandatory",
+      emailNotAllowedMessage: "Please enter a valid email",
+      emailMinLengthMessage: "The number of characters must be at least 2",
+      emailMaxLengthMessage: "The number of characters must not exceed 30",
+      themeMinLengthMessage: "The number of characters must be at least 6",
+      themeMaxLengthMessage: "The number of characters must not exceed 30",
+      messageMaxLengthMessage: "The number of characters must not exceed 600",
+    },
+  },
+  ua: {
+    schema: {
+      nameInvalidName: "Введіть, будь ласка, коректне ім’я",
+      nameNotAllowedMessage:
+        'Заборонено використовувати такі символи: «% ^ * | ~ {} ; "<>. , /»',
+      nameMinLengthMessage: "Кількість символів має бути не менше 2",
+      nameMaxLengthMessage: "Кількість символів має бути не більше 30",
+      requiredMessage: "Це поле обов'язкове для заповнення",
+      emailNotAllowedMessage: "Введіть, будь ласка, коректний email",
+      emailMinLengthMessage: "Кількість символів має бути не менше 6",
+      emailMaxLengthMessage: "Кількість символів має бути не більше 30",
+      themeMinLengthMessage: "Кількість символів має бути не менше 6",
+      themeMaxLengthMessage: "Кількість символів має бути не більше 30",
+      messageMaxLengthMessage: "Кількість символів має бути не більше 600",
+    },
+  },
+};
+export default translations;
