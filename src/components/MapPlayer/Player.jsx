6f0330e69f87a3c2a3e82fc53086b0de75b3af41
IrynaShashko <95182459+IrynaShashko@users.noreply.github.com>
2023-09-30 10:19:30 +0300

    feat: add active button for map tabs

--- src/components/MapPlayer/Player.jsx
+++ src/components/MapPlayer/Player.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Player.css";
 import classNames from "classnames";
 import { FiShare2, FiShuffle, FiRefreshCw } from "react-icons/fi";
 import { BsFillSkipEndFill, BsFillSkipStartFill, BsPlayFill, BsPauseFill } from "react-icons/bs";
 import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
+import { use } from "i18next";
 
 export const Player = ({
   isLightTheme,
@@ -21,6 +22,17 @@ export const Player = ({
   const playStopToggle = () => {
     setIsPlaying(!isPlaying);
   };
+
+  useEffect(() => {
+    const buttonMap = document.getElementById("map-tab");
+    if (buttonMap) {
+      buttonMap.classList.add("active-btn");
+
+      return () => {
+        buttonMap.classList.remove("active-btn");
+      };
+    }
+  }, [])
 
   const nextSongIndex = (currentSongIndex + 1) % playlist.length;
   const previousSongIndex = (currentSongIndex - 1) % playlist.length;
@@ -47,9 +59,9 @@ export const Player = ({
   return (
     <div className="map-player">
       <div
-        className={ classNames("map-player-navigation-wrapper", {
+        className={classNames("map-player-navigation-wrapper", {
           "map-player-navigation-wrapper-light": isLightTheme,
-        }) }
+        })}
       >
         <div className="map-player-secondary-buttons-left">
           <FiShare2 className="map-player-share-button" />
@@ -57,26 +69,26 @@ export const Player = ({
         </div>
         <div className="map-player-primary-buttons-group">
           <button
-            className={ classNames("map-player-previous-button", {
+            className={classNames("map-player-previous-button", {
               "map-player-previous-button-light": isLightTheme,
-            }) }
-            onClick={ handlePreviousSong }
+            })}
+            onClick={handlePreviousSong}
           >
             <BsFillSkipStartFill />
           </button>
           <button
-            className={ classNames("map-player-play-pause-button", {
+            className={classNames("map-player-play-pause-button", {
               "map-player-play-pause-button-light": isLightTheme,
-            }) }
-            onClick={ playStopToggle }
+            })}
+            onClick={playStopToggle}
           >
-            { !isPlaying ? <BsPlayFill /> : <BsPauseFill style={ { fill: "var(--red-700)" } } /> }
+            {!isPlaying ? <BsPlayFill /> : <BsPauseFill style={{ fill: "var(--red-700)" }} />}
           </button>
           <button
-            className={ classNames("map-player-next-button", {
+            className={classNames("map-player-next-button", {
               "map-player-next-button-light": isLightTheme,
-            }) }
-            onClick={ handleNextSong }
+            })}
+            onClick={handleNextSong}
           >
             <BsFillSkipEndFill />
           </button>
@@ -84,23 +96,23 @@ export const Player = ({
         <div className="map-player-secondary-buttons-right">
           <FiRefreshCw
             className="map-player-refresh-button"
-            onClick={ handleLoop }
-            style={ isLooped && { color: "var(--red-700)" } }
+            onClick={handleLoop}
+            style={isLooped && { color: "var(--red-700)" }}
           />
           <div className="map-player-volume-wrapper">
-            { volume > 0 ? (
-              <HiVolumeUp className="map-player-volume-button" onClick={ handleMute } />
+            {volume > 0 ? (
+              <HiVolumeUp className="map-player-volume-button" onClick={handleMute} />
             ) : (
-              <HiVolumeOff className="map-player-volume-button" onClick={ handleMute } />
-            ) }
+              <HiVolumeOff className="map-player-volume-button" onClick={handleMute} />
+            )}
             <input
               type="range"
               id="mapVolumeInputId"
-              min={ 0 }
-              max={ 1 }
-              step={ 0.01 }
-              value={ volume }
-              onChange={ handleVolumeChange }
+              min={0}
+              max={1}
+              step={0.01}
+              value={volume}
+              onChange={handleVolumeChange}
             />
           </div>
         </div>
