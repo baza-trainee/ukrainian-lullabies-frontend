f359408a35ba0309cc496c6abda2658decb90543
Diana Tuz <dianatuz12@gmail.com>
2023-09-30 17:39:19 +0300

    some changes

--- src/components/MapPlayer/MapPlayer.jsx
+++ src/components/MapPlayer/MapPlayer.jsx
@@ -2,73 +2,90 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import classNames from "classnames";
 import { useTranslation } from "react-i18next";
-import ReactPlayer from "react-player/youtube";
+import ReactAudioPlayer from 'react-audio-player';
 import { Player } from "./Player";
 import { selectData, selectLoading } from "../../redux/DataSlice";
 import { fetchData } from "../../redux/Lullabies/fetchLullabies";
 import { useParams } from "react-router-dom";
 import { BsRepeat } from "react-icons/bs";
+import { setCurrentUrl, setCurrentLyrics, setCurrentId, setCurrentName } from "../../redux/currentSong/currentSongSlice";
 import { PauseCircleIconDark } from "../../icons/SelectionsIcons/PauseCircleIcon";
 import { PlayCircleIconDark } from "../../icons/SelectionsIcons/PlayCircleIcon";
 import './MapPlayer.css';
 
+
 const songsData = [
   {
     id: 0,
-    url: "https://youtu.be/J1l9fbPeEic",
-    name: "Ой ну коту волохатий | Наспів з Полтавщини",
-    watches: 100,
-    duration: "0:32",
+    url: "https://deti.e-papa.com.ua/mpf/9211814143.mp3",
+    name: "Колискова для мами",
+    duration: "3:02",
+    lyrics: 'колискова для мами слова'
   },
   {
     id: 1,
-    url: "https://youtu.be/UcqNIuiP-SM",
-    name: "Ой люля, люля, мій малесенький синульку | Наспів з Західного Полісся",
+    url: "https://deti.e-papa.com.ua/mpf/17146805.mp3",
+    name: "Ходить сон бiля вiкон",
     watches: 1500,
     duration: "1:27",
+    lyrics: 'Ходить сон бiля вiкон',
+
   },
   {
     id: 2,
-    url: "https://youtu.be/_ecx-2oPvAc",
-    name: "Ой ходить сон коло вікон | Наспів з Поділля",
+    url: "https://deti.e-papa.com.ua/mpf/9211811816.mp3",
+    name: "Котику сіренький",
     watches: 2000,
     duration: "1:07",
+    lyrics: 'Котику сіренький текст',
+
+  },
+  {
+    id: 3,
+    url: "https://deti.e-papa.com.ua/mpf/921180978.mp3",
+    name: "Колискова",
+    watches: 2000,
+    duration: "1:07",
+    lyrics: 'Котику сіренький текст',
   },
 ];
+
 
 export const MapPlayer = () => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
-  const loading = useSelector(selectLoading);
-  const data = useSelector(selectData);
+  const data = songsData;
 
   const currentUrl = useSelector((state) => state.currentSong.currentUrl);
   const currentName = useSelector((state) => state.currentSong.currentName);
   const currentId = useSelector((state) => state.currentSong.currentId);
   const currentLyrics = useSelector((state) => state.currentSong.currentLyrics);
-
   const [currentVideoIndex, setCurrentVideoIndex] = useState(currentId);
   const [currentVideoUrl, setCurrentVideoUrl] = useState(currentUrl);
-
-  const { url } = useParams();
-
-  useEffect(() => {
-    dispatch(fetchData());
-    if (url)
-    {
-      setCurrentVideoUrl(decodeURIComponent(url));
-    }
-  }, [dispatch, url]);
 
   const isLightTheme = useSelector((state) => state.theme.isLightTheme);
 
   const [isPlaying, setIsPlaying] = useState(false);
   const [isLooped, setIsLooped] = useState(false);
   const [volume, setVolume] = useState(0.5);
-  const [playlist] = useState(data);
 
+  useEffect(() => {
+    // dispatch(fetchData(songsData));
+
+    const savedSong = JSON.parse(localStorage.getItem('currentSong'));
+    if (savedSong)
+    {
+      dispatch(setCurrentUrl(savedSong.url));
+      dispatch(setCurrentLyrics(savedSong.lyrics));
+      dispatch(setCurrentId(savedSong.id));
+      dispatch(setCurrentName(savedSong.name));
+    }
+  }, [dispatch]);
 
   const playPauseSong = (url) => {
+    const newIndex = data.findIndex((song) => song.url === url);
+    setCurrentVideoIndex(newIndex);
+
     if (!isPlaying && currentVideoUrl === url)
     {
       setIsPlaying(true);
@@ -85,13 +102,19 @@ export const MapPlayer = () => {
       setCurrentVideoUrl(url);
       setIsLooped(false);
     }
-
-    setCurrentVideoIndex(playlist.findIndex((song) => song.url === url));
   };
 
-  console.log(currentLyrics);
   const handleLoop = () => {
     setIsLooped(!isLooped);
+  };
+
+  const handleVideoChange = (url, id, lyrics, name) => {
+    dispatch(setCurrentUrl(url));
+    dispatch(setCurrentLyrics(lyrics));
+    dispatch(setCurrentId(id));
+    dispatch(setCurrentName(name));
+
+    localStorage.setItem('currentSong', JSON.stringify({ url, id, lyrics, name }));
   };
 
   return (
@@ -100,14 +123,12 @@ export const MapPlayer = () => {
       </h2>
       <div className="map-player-wrapper container margin-bottom">
         <div className="map-player_container">
-          <ReactPlayer
-            url={ currentVideoUrl }
-            width="100%"
-            height="60%"
-            playing={ isPlaying }
+          <ReactAudioPlayer
+            src={ currentUrl }
+            autoPlay={ isPlaying }
             onEnded={ () => setIsPlaying(false) }
-            loop={ isLooped }
             volume={ volume }
+            loop={ isLooped }
           />
           <h3 className="current-name text-l">
             { currentName }
@@ -117,7 +138,7 @@ export const MapPlayer = () => {
             isPlaying={ isPlaying }
             setIsPlaying={ setIsPlaying }
             setcurrentVideoUrl={ setCurrentVideoUrl }
-            playlist={ playlist }
+            playlist={ data }
             currentVideoIndex={ currentVideoIndex }
             setcurrentVideoIndex={ setCurrentVideoIndex }
             handleLoop={ handleLoop }
@@ -133,38 +154,75 @@ export const MapPlayer = () => {
         </div>
         <div className="map-player_playlist">
           <p className="text-l text-margin">Колекція музею</p>
-          <ul className=" ">
-            { playlist.map(({ name, id, url, }, duration = 2) => (
-              <li
-                key={ id }
-                className={ classNames("map-player_card", { "map-player_card-light": isLightTheme }) }
-              >
+          <ul className=" map-player_playlist">
+            {
+              data.map(({ name, id, url, lyrics, duration }) => (
+                <li
+                  key={ id }
+                  className={ classNames("map-player_card", { "map-player_card-light": isLightTheme }) }
+                  onClick={ () => handleVideoChange(url, id, lyrics, name) }
+                >
 
-                <div className="playlist-item ">
-                  <button
-                    className={ classNames("selections-playlist-item-play-pause-button", "selection-playlist-button", {
-                      "selections-playlist-item-play-pause-button-light": isLightTheme,
-                    }) }
-                    onClick={ () => playPauseSong(url) }
-                  >
-                    { isPlaying && url === currentVideoUrl ? <PauseCircleIconDark /> : <PlayCircleIconDark /> }
-                  </button>
+                  <div className="playlist-item ">
+                    <button
+                      className={ classNames("selections-playlist-item-play-pause-button", "selection-playlist-button", {
+                        "selections-playlist-item-play-pause-button-light": isLightTheme,
+                      }) }
+                      onClick={ () => playPauseSong(url) }
+                    >
+                      { isPlaying && url === currentVideoUrl ? <PauseCircleIconDark /> : <PlayCircleIconDark /> }
+                    </button>
 
-                  <span className="selections-playlist-item-name">{ name.toUpperCase().slice(0, 50) }</span>
-                </div>
-                <div className="playlist-item">
-                  <span className="selections-playlist-item-duration text-xs-bold">{ duration }</span>
-                  <button
-                    className="selections-playlist-item-repeat-button selection-playlist-button"
-                    onClick={ handleLoop }
-                    disabled={ currentVideoUrl !== url }
-                  >
-                    <BsRepeat style={ isLooped && currentUrl === url && { fill: "var(--red-700)" } } />
-                  </button>
-                </div>
-              </li>
+                    <span className="selections-playlist-item-name">{ name.toUpperCase().slice(0, 50) }</span>
+                  </div>
+                  <div className="playlist-item">
+                    <span className="selections-playlist-item-duration text-xs-bold">{ duration }</span>
+                    <button
+                      className="selections-playlist-item-repeat-button selection-playlist-button"
+                      onClick={ handleLoop }
+                      disabled={ currentVideoUrl !== url }
+                    >
+                      <BsRepeat style={ isLooped && currentUrl === url && { fill: "var(--red-700)" } } />
+                    </button>
+                  </div>
+                </li>
+              ))
+            }ata.map(({ name, id, url, lyrics }
+              // , duration = 2
+            ) => (
+            <li
+              key={ id }
+              className={ classNames("map-player_card", { "map-player_card-light": isLightTheme }) }
+              onClick={ () => handleVideoChange(url, id, lyrics, name) }
+            >
+
+              <div className="playlist-item ">
+                <button
+                  className={ classNames("selections-playlist-item-play-pause-button", "selection-playlist-button", {
+                    "selections-playlist-item-play-pause-button-light": isLightTheme,
+                  }) }
+                  onClick={ () => playPauseSong(url) }
+                >
+                  { isPlaying && url === currentVideoUrl ? <PauseCircleIconDark /> : <PlayCircleIconDark /> }
+                </button>
+
+                <span className="selections-playlist-item-name">{ name.toUpperCase().slice(0, 50) }</span>
+              </div>
+              <div className="playlist-item">
+                <span className="selections-playlist-item-duration text-xs-bold">01:12</span>
+                <button
+                  className="selections-playlist-item-repeat-button selection-playlist-button"
+                  onClick={ handleLoop }
+                  disabled={ currentVideoUrl !== url }
+                >
+                  <BsRepeat style={ isLooped && currentUrl === url && { fill: "var(--red-700)" } } />
+                </button>
+              </div>
+            </li>
             )) }
-            { playlist.map(({ name, id, url, }, duration = 2) => (
+            { data.map(({ name, id, url, }
+              // duration = '00:37'
+            ) => (
               <li
                 key={ id }
                 className={ classNames("map-player_card", { "map-player_card-light": isLightTheme }) }
@@ -183,7 +241,7 @@ export const MapPlayer = () => {
                   <span className="selections-playlist-item-name">{ name.toUpperCase().slice(0, 50) }</span>
                 </div>
                 <div className="playlist-item">
-                  <span className="selections-playlist-item-duration text-xs-bold">{ duration }</span>
+                  <span className="selections-playlist-item-duration text-xs-bold">00:37</span>
                   <button
                     className="selections-playlist-item-repeat-button selection-playlist-button"
                     onClick={ handleLoop }
--- src/components/MapPlaylist/MapPlaylist.jsx
+++ src/components/MapPlaylist/MapPlaylist.jsx
@@ -1,4 +1,64 @@
-import React, { useEffect } from "react";
+// import React, { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { fetchData } from "../../redux/Lullabies/fetchLullabies";
+// import { selectData, selectLoading } from "../../redux/DataSlice";
+// import './MapPlaylist.css';
+// import classNames from "classnames";
+// import { setCurrentUrl, setCurrentLyrics, setCurrentId, setCurrentName } from "../../redux/currentSong/currentSongSlice";
+// import { Link } from "react-router-dom";
+
+// export const MapPlaylist = () => {
+//   const dispatch = useDispatch();
+//   const loading = useSelector(selectLoading);
+//   const data = useSelector(selectData);
+
+//   useEffect(() => {
+//     dispatch(fetchData());
+//   }, [dispatch]);
+
+//   const handleVideoChange = (url, id, lyrics, name) => {
+//     dispatch(setCurrentUrl(url));
+//     dispatch(setCurrentLyrics(lyrics));
+//     dispatch(setCurrentId(id));
+//     dispatch(setCurrentName(name));
+
+//     localStorage.setItem('currentSong', JSON.stringify({ url, id, lyrics, name }));
+//   };
+
+//   return (
+//     !loading && data && (
+//       <section id="anima" className="playlist margin-bottom">
+//         <div className="map-playlist_container"></div>
+
+//         <div className="map-player_wrap">
+//           <div className={ classNames('map-player_playlist scroll') }>
+//             <p className="text-l text-margin">Колекція музею</p>
+//             <ul>
+//               { data.map(({ name, id, url, lyrics }) => (
+//                 <li
+//                   key={ id }
+//                   className={ classNames('map-player_card') }
+//                 >
+//                   <Link
+//                     to={ `/player` }
+//                     className="map-player_card-link"
+//                     onClick={ () => handleVideoChange(url, id, lyrics, name) }
+//                   >
+//                     <div className="map-player_card-text">
+//                       <div className={ classNames('play') }></div>
+//                       <p className="map-player_card-title text-sm">{ name }</p>
+//                     </div>
+//                   </Link>
+//                 </li>
+//               )) }
+//             </ul>
+//           </div>
+//         </div>
+//       </section>
+//     )
+//   );
+// };
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchData } from "../../redux/Lullabies/fetchLullabies";
 import { selectData, selectLoading } from "../../redux/DataSlice";
@@ -7,21 +67,50 @@ import classNames from "classnames";
 import { setCurrentUrl, setCurrentLyrics, setCurrentId, setCurrentName } from "../../redux/currentSong/currentSongSlice";
 import { Link } from "react-router-dom";
 
+const songsData = [
+  {
+    id: 0,
+    url: "https://deti.e-papa.com.ua/mpf/9211814143.mp3",
+    name: "Колискова для мами",
+    lyrics: 'колискова для мами слова'
+  },
+  {
+    id: 1,
+    url: "https://deti.e-papa.com.ua/mpf/17146805.mp3",
+    name: "Ходить сон бiля вiкон",
+    lyrics: 'Ходить сон бiля вiкон',
+  },
+  {
+    id: 2,
+    url: "https://deti.e-papa.com.ua/mpf/9211811816.mp3",
+    name: "Котику сіренький",
+    lyrics: 'Котику сіренький текст',
+  },
+  {
+    id: 3,
+    url: "https://deti.e-papa.com.ua/mpf/921180978.mp3",
+    name: "Колискова",
+    lyrics: 'Котику сіренький текст',
+  },
+];
+
 export const MapPlaylist = () => {
   const dispatch = useDispatch();
   const loading = useSelector(selectLoading);
-  const data = useSelector(selectData);
-
-  const handleVideoChange = (url, id, lyrics, name) => {
-    dispatch(setCurrentUrl(url));
-    dispatch(setCurrentLyrics(lyrics));
-    dispatch(setCurrentId(id));
-    dispatch(setCurrentName(name));
-  };
+  const data = songsData; // Замінити `data` на ваш тестовий масив `songsData`
 
   useEffect(() => {
     dispatch(fetchData());
   }, [dispatch]);
+
+  const handleAudioChange = (url, id, lyrics, name) => {
+    dispatch(setCurrentUrl(url));
+    dispatch(setCurrentLyrics(lyrics));
+    dispatch(setCurrentId(id));
+    dispatch(setCurrentName(name));
+
+    localStorage.setItem('currentSong', JSON.stringify({ url, id, lyrics, name }));
+  };
 
   return (
     !loading && data && (
@@ -40,7 +129,7 @@ export const MapPlaylist = () => {
                   <Link
                     to={ `/player` }
                     className="map-player_card-link"
-                    onClick={ handleVideoChange(url, id, lyrics, name) }
+                    onClick={ () => handleAudioChange(url, id, lyrics, name) }
                   >
                     <div className="map-player_card-text">
                       <div className={ classNames('play') }></div>
