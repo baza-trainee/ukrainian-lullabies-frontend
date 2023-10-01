c51afa9718428105374f55bcfb7aa237d88376b1
Olena <newsroommy@gmail.com>
2023-10-01 10:01:29 +0300

    add

--- src/components/Selections/Selections.jsx
+++ src/components/Selections/Selections.jsx
@@ -91,19 +91,15 @@ export const Selections = () => {
   // ----+++---+---
 
   const playPauseSong = (url) => {
-    if (!isPlaying && currentSong === url)
-    {
+    if (!isPlaying && currentSong === url) {
       setIsPlaying(true);
-    } else if (!isPlaying)
-    {
+    } else if (!isPlaying) {
       setCurrentSong(url);
       setIsPlaying(true);
       setIsLooped(false);
-    } else if (isPlaying && currentSong == url)
-    {
+    } else if (isPlaying && currentSong == url) {
       setIsPlaying(false);
-    } else
-    {
+    } else {
       setCurrentSong(url);
       setIsLooped(false);
     }
@@ -121,11 +117,9 @@ export const Selections = () => {
 
   const handleNextSong = () => {
     // its own function, we have similar in SelectionsPlayer
-    if (isPlaylistShuffled)
-    {
+    if (isPlaylistShuffled) {
       playRandomSong();
-    } else
-    {
+    } else {
       const nextSongIndex = (currentSongIndex + 1) % playlist.length;
       setCurrentSong(playlist[nextSongIndex].url);
       setCurrentSongIndex(nextSongIndex);
@@ -137,8 +131,7 @@ export const Selections = () => {
     const max = playlist.length - 1;
 
     let newIndex;
-    do
-    {
+    do {
       newIndex = Math.floor(Math.random() * (max - min + 1)) + min;
     } while (newIndex === currentSongIndex);
 
@@ -199,69 +192,100 @@ export const Selections = () => {
       <ReactPlayer
         width="0px"
         height="0px"
-        ref={ reactPlayerRef }
-        url={ currentSong }
-        playing={ isPlaying }
-        onEnded={ () =>
+        ref={reactPlayerRef}
+        url={currentSong}
+        playing={isPlaying}
+        onEnded={() =>
           isPlaylistLooped ? handleNextSong() : setIsPlaying(false)
         }
-        loop={ isLooped }
-        volume={ volume }
+        loop={isLooped}
+        volume={volume}
       />
-      <h2 className="selections-title text-4xl">{ t("selection") }</h2>
+      <h2 className="selections-title text-4xl">{t("selection")}</h2>
       <div className="selections-wrapper container margin-bottom">
         <div className="selections-image">
-          <img src={ favoriteSongFirst } alt="song covering" />
+          <img src={favoriteSongFirst} alt="song covering" />
         </div>
         <div className="selections-info">
           <div className="selections-info-about">
             <h4 className="selections-info-title text-2xl">
-              { t("ukrainianLullabies") }
+              {t("ukrainianLullabies")}
             </h4>
-            <p className="selections-info-text text-base">{ t("lullabySong") }</p>
+            <p className="selections-info-text text-base">{t("lullabySong")}</p>
           </div>
-          { !playlistError ? (
+          {!playlistError ? (
             <ul className="selections-playlist-list">
-              { playlist &&
+              {playlist &&
                 playlist.map((item, index) => (
                   <li
-                    className={ classNames("selections-playlist-list-item", {
+                    className={classNames("selections-playlist-list-item", {
                       "selections-playlist-list-item-light": isLightTheme,
-                      "selections-playlist-list-item-active": item.url === currentSong,
-                      "selections-playlist-list-item-active-light": isLightTheme && item.url === currentSong,
-                    }) }
-                    key={ index }
-                    onClick={ () => playPauseSong(item.url) }
+                      "selections-playlist-list-item-active":
+                        item.url === currentSong,
+                      "selections-playlist-list-item-active-light":
+                        isLightTheme && item.url === currentSong,
+                    })}
+                    key={index}
+                    onClick={() => playPauseSong(item.url)}
                   >
                     <span className="selections-playlist-item-number">
-                      { isPlaying && item.url === currentSong ? <SoundWaveIcon /> : index + 1 }
+                      {isPlaying && item.url === currentSong ? (
+                        <SoundWaveIcon />
+                      ) : (
+                        index + 1
+                      )}
                     </span>
                     <div className="selection-playlist-playBtn-name-group">
                       <button
-                        className={ classNames("selections-playlist-item-play-pause-button", "selection-playlist-button", {
-                          "selections-playlist-item-play-pause-button-light": isLightTheme,
-                        }) }
-                        onClick={ () => playPauseSong(item.url) }
+                        className={classNames(
+                          "selections-playlist-item-play-pause-button",
+                          "selection-playlist-button",
+                          {
+                            "selections-playlist-item-play-pause-button-light":
+                              isLightTheme,
+                          }
+                        )}
+                        onClick={() => playPauseSong(item.url)}
                       >
-                        { isPlaying && item.url === currentSong ? <PauseCircleIconDark /> : <PlayCircleIconDark /> }
+                        {isPlaying && item.url === currentSong ? (
+                          <PauseCircleIconDark />
+                        ) : (
+                          <PlayCircleIconDark />
+                        )}
                       </button>
 
-                      <span className="selections-playlist-item-name">{ item.name.toUpperCase().slice(0, 50) }</span>
+                      <span className="selections-playlist-item-name">
+                        {item.name.toUpperCase().slice(0, 50)}
+                      </span>
                     </div>
-                    {/* selections with dropdown for mobile */ }
+                    {/* selections with dropdown for mobile */}
                     <div className="selections-playlist-item-group">
-                      <span className="selections-playlist-item-duration text-xs-bold">{ item.duration }</span>
+                      <span className="selections-playlist-item-duration text-xs-bold">
+                        {item.duration}
+                      </span>
                       <button
-                        className={ classNames("selections-playlist-item-repeat-button", "selection-playlist-button", {
-                          "selections-playlist-item-repeat-button-light": isLightTheme,
-                        }) }
-                        onClick={ (e) => {
+                        className={classNames(
+                          "selections-playlist-item-repeat-button",
+                          "selection-playlist-button",
+                          {
+                            "selections-playlist-item-repeat-button-light":
+                              isLightTheme,
+                          }
+                        )}
+                        onClick={(e) => {
                           e.stopPropagation();
                           handleLoop();
-                        } }
-                        disabled={ currentSong !== item.url }
+                        }}
+                        disabled={currentSong !== item.url}
                       >
-                        <BsRepeat style={ isLooped && currentSong === item.url && { fill: "var(--red-700)" } } />
+                        <BsRepeat
+                          style={
+                            isLooped &&
+                            currentSong === item.url && {
+                              fill: "var(--red-700)",
+                            }
+                          }
+                        />
                       </button>
                       {/* <button
                     className="selections-playlist-item-like-button selection-playlist-button"
@@ -314,29 +338,30 @@ export const Selections = () => {
                   </button>
                 </div> */}
                   </li>
-                )) }
+                ))}
             </ul>
           ) : (
-            <div className="selections-playlist-error text-l">Error: { playlistError }</div>
-          ) }
-
+            <div className="selections-playlist-error text-l">
+              Error: {playlistError}
+            </div>
+          )}
           <SelectionsPlayer
-            isLightTheme={ isLightTheme }
-            isPlaying={ isPlaying }
-            setIsPlaying={ setIsPlaying }
-            setCurrentSong={ setCurrentSong }
-            playlist={ playlist }
-            currentSongIndex={ currentSongIndex }
-            setCurrentSongIndex={ setCurrentSongIndex }
-            isPlaylistLooped={ isPlaylistLooped }
-            handleLoopPlaylist={ handleLoopPlaylist }
-            isPlaylistShuffled={ isPlaylistShuffled }
-            setIsPlaylistShuffled={ setIsPlaylistShuffled }
-            playRandomSong={ playRandomSong }
-            volume={ volume }
-            setVolume={ setVolume }
-            previousVolume={ previousVolume }
-            setPreviousVolume={ setPreviousVolume }
+            isLightTheme={isLightTheme}
+            isPlaying={isPlaying}
+            setIsPlaying={setIsPlaying}
+            setCurrentSong={setCurrentSong}
+            playlist={playlist}
+            currentSongIndex={currentSongIndex}
+            setCurrentSongIndex={setCurrentSongIndex}
+            isPlaylistLooped={isPlaylistLooped}
+            handleLoopPlaylist={handleLoopPlaylist}
+            isPlaylistShuffled={isPlaylistShuffled}
+            setIsPlaylistShuffled={setIsPlaylistShuffled}
+            playRandomSong={playRandomSong}
+            volume={volume}
+            setVolume={setVolume}
+            previousVolume={previousVolume}
+            setPreviousVolume={setPreviousVolume}
           />
         </div>
       </div>
