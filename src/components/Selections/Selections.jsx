3c2132880d616dd234c9bd6d5a3d162e5d134b40
Olena Bondarenko <107562787+OllBond@users.noreply.github.com>
2023-10-02 18:39:47 +0300

    Fix/hero and pop up (#126)
    * add feedBackForm
    
    * fix: css pop-up
    
    * fix: css pop-up
    
    * add setTimeout
    
    * add en language
    
    * add en for placeholder and fix css in popUpFeedBack
    
    * fix pop-up in form add overlay
    
    * fix: css pop-up
    
    * fix: css pop-up
    
    * add setTimeout
    
    * add en language
    
    * add en for placeholder and fix css in popUpFeedBack
    
    * add
    
    * fix pop-up in button share

--- src/components/Selections/Selections.jsx
+++ src/components/Selections/Selections.jsx
@@ -163,43 +163,72 @@ export const Selections = () => {
         </div>
         <div className="selections-info">
           <div className="selections-info-about">
-
-            <h4 className="selections-info-title text-2xl">{t("ukrainianLullabies")}</h4>
+            <h4 className="selections-info-title text-2xl">
+              {t("ukrainianLullabies")}
+            </h4>
             <p className="selections-info-text text-base">{t("lullabySong")}</p>
           </div>
           <ul className="selections-playlist-list">
             {playlist.map((item, index) => (
               <li
-                className={classNames("selections-playlist-list-item", { "selections-playlist-list-item-light": isLightTheme })}
+                className={classNames("selections-playlist-list-item", {
+                  "selections-playlist-list-item-light": isLightTheme,
+                })}
                 key={index}
               >
                 <span className="selections-playlist-item-number">
-                  {isPlaying && item.url === currentSong ? <SoundWaveIcon /> : index + 1}
+                  {isPlaying && item.url === currentSong ? (
+                    <SoundWaveIcon />
+                  ) : (
+                    index + 1
+                  )}
                 </span>
                 <div className="selection-playlist-playBtn-name-group">
                   <button
-                    className={classNames("selections-playlist-item-play-pause-button", "selection-playlist-button", {
-                      "selections-playlist-item-play-pause-button-light": isLightTheme,
-                    })}
+                    className={classNames(
+                      "selections-playlist-item-play-pause-button",
+                      "selection-playlist-button",
+                      {
+                        "selections-playlist-item-play-pause-button-light":
+                          isLightTheme,
+                      }
+                    )}
                     onClick={() => playPauseSong(item.url)}
                   >
-                    {isPlaying && item.url === currentSong ? <PauseCircleIconDark /> : <PlayCircleIconDark />}
+                    {isPlaying && item.url === currentSong ? (
+                      <PauseCircleIconDark />
+                    ) : (
+                      <PlayCircleIconDark />
+                    )}
                   </button>
 
-                  <span className="selections-playlist-item-name">{item.name.toUpperCase().slice(0, 50)}</span>
+                  <span className="selections-playlist-item-name">
+                    {item.name.toUpperCase().slice(0, 50)}
+                  </span>
                 </div>
                 {/* selections with dropdown for mobile */}
                 <div className="selections-playlist-item-group">
-                  <span className="selections-playlist-item-duration text-xs-bold">{item.duration}</span>
+                  <span className="selections-playlist-item-duration text-xs-bold">
+                    {item.duration}
+                  </span>
                   <button
-                    className={classNames("selections-playlist-item-repeat-button", "selection-playlist-button", {
-                      "selections-playlist-item-repeat-button-light": isLightTheme,
-                    })}
+                    className={classNames(
+                      "selections-playlist-item-repeat-button",
+                      "selection-playlist-button",
+                      {
+                        "selections-playlist-item-repeat-button-light":
+                          isLightTheme,
+                      }
+                    )}
                     onClick={handleLoop}
                     disabled={currentSong !== item.url}
                   >
-                    <BsRepeat style={isLooped && currentSong === item.url && { fill: "var(--red-700)" }} />
-
+                    <BsRepeat
+                      style={
+                        isLooped &&
+                        currentSong === item.url && { fill: "var(--red-700)" }
+                      }
+                    />
                   </button>
                   {/* <button
                     className="selections-playlist-item-like-button selection-playlist-button"
@@ -271,9 +300,16 @@ export const Selections = () => {
           />
         </div>
       </div>
-      <img src={endSectionOrnamentDesktop} alt="ornament" className="selections-ornament-desktop" />
-      <img src={endSectionOrnamentMobile} alt="ornament" className="selections-ornament-mobile" />
-
+      <img
+        src={endSectionOrnamentDesktop}
+        alt="ornament"
+        className="selections-ornament-desktop"
+      />
+      <img
+        src={endSectionOrnamentMobile}
+        alt="ornament"
+        className="selections-ornament-mobile"
+      />
     </div>
   );
 };
