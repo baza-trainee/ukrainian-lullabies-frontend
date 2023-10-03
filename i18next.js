081bcc4b28391fb7058421790ef455b2fbd4e7c0
BodNS <71136538+BodNS@users.noreply.github.com>
2023-10-03 19:38:48 +0200

    Fix bugs (#130)
    * commit packege-lock.json
    
    * fix Chatbot's bugs
    
    * fix AboutUs's bugs

--- i18next.js
+++ i18next.js
@@ -51,13 +51,13 @@ i18n
           projectDescription:
             "The purpose of our project is to create a media platform where you can explore lullabies from all regions of Ukraine (Ukrainians and national minorities), learn the songs to sing to your child, or create your own lullaby playlist for playback.",
           projectDescription2:
-            "For the implementation of the 'Ukrainian Lullabies' project, scientists from the Research Laboratory of Ethnomusicology of the National Academy of Music of Ukraine and IT volunteers from the 'Baza Trainee Ukraine' platform have joined forces. They presented the project at Hatathon 4.0: Ukraine Heritage Edition, where it became one of the best in the field of preserving intangible cultural heritage. They established the public organization 'Lullaby Museum' and are working together to create a media resource with lullaby songs for mothers, children, and families in Ukraine and abroad.",
+            "For the implementation of the \"Ukrainian Lullabies\" project, scientists from the Research Laboratory of Ethnomusicology of the National Academy of Music of Ukraine and IT volunteers from the \"Baza Trainee Ukraine\" platform have joined forces. They presented the project at Hatathon 4.0: Ukraine Heritage Edition, where it became one of the best in the field of preserving intangible cultural heritage. They established the public organization \"Lullaby Museum\" and are working together to create a media resource with lullaby songs for mothers, children, and families in Ukraine and abroad.",
           supportUs: "You can support us",
           supportDescription:
-            "You can support the development of the 'Ukrainian Lullabies' project and stay up to date with the latest news. Get nice bonuses by subscribing to the project on Patreon or Buy Me a Coffee.",
+            "You can support the development of the \"Ukrainian Lullabies\" project and stay up to date with the latest news. Get nice bonuses by subscribing to the project on Patreon or Buy Me a Coffee.",
           supportDescription2:
-            "Your donations will be used to cover the costs of an AWS server for storing unique recordings of Ukrainian authenticity, hosting for the platform, scientific and technical support, legal support for the 'Ukrainian Lullabies' project, as well as funding new expeditions and recordings.",
-          supportDescription3: "With respect, the 'Ukrainian Lullabies' project team",
+            "Your donations will be used to cover the costs of an AWS server for storing unique recordings of Ukrainian authenticity, hosting for the platform, scientific and technical support, legal support for the \"Ukrainian Lullabies\" project, as well as funding new expeditions and recordings.",
+          supportDescription3: "With respect, the \"Ukrainian Lullabies\" project team.",
           ourPartners: "Our partners",
           feedbackForm: "Feedback form",
           contactUs: "If you have any questions/suggestions/wishes - we look forward to hearing from you.",
@@ -144,13 +144,13 @@ i18n
           projectDescription:
             "Мета нашого проєкту - створення медіаплатформи, на якій Ви зможете ознайомитися з колисковими піснями з усіх регіонів України (українців та національних меншин), вивчити пісні, щоб співати своїй дитині або створити власний play-лист з колисковими, щоб програвати його.",
           projectDescription2:
-            "Для реалізації проєкту 'Українські колискові' науковці з Науково-дослідної лабораторії етномузикології Національної музичної академії України та IT-волонтери з платформи 'Baza Trainee Ukraine' об'єднались в команду і представили проєкт на Hatathon 4.0: Ukraine Heritage Edition, де він став одним з найкращих в галузі збереження нематеріальної культурної спадщини, створили громадську організацію 'Музей колискової' та разом працюють над створенням медіаресурсу з колисковими піснями для мам і дітей, для родин в Україні і за кордоном.",
+            "Для реалізації проєкту \"Українські колискові\" науковці з Науково-дослідної лабораторії етномузикології Національної музичної академії України та IT-волонтери з платформи \"Baza Trainee Ukraine\" об'єднались в команду і представили проєкт на Hatathon 4.0: Ukraine Heritage Edition, де він став одним з найкращих в галузі збереження нематеріальної культурної спадщини, створили громадську організацію \"Музей колискової\" та разом працюють над створенням медіаресурсу з колисковими піснями для мам і дітей, для родин в Україні і за кордоном.",
           supportUs: "Ви можете підтримати нас",
           supportDescription:
-            "Ви можете підтримати розвиток проєкту 'Українські колискові' і бути в курсі перших новин, отримати приємні бонуси, оформивши підписку на проєкт на Patreon або Buy me a Coffee.",
+            "Ви можете підтримати розвиток проєкту \"Українські колискові\" і бути в курсі перших новин, отримати приємні бонуси, оформивши підписку на проєкт на Patreon або Buy me a Coffee.",
           supportDescription2:
             " Донати підуть на поповнення колекції колискових та забезпечення діяльності платформи: на оплату сервера на AWS для збереження унікальних записів української автентики, на оплату хостингу для платформи, на забезпечення наукового та технічного супроводу, юридичної підтримки проєкту і – на нові експедиції та записи!",
-          supportDescription3: " З повагою, команда проєкту 'Українські колискові'",
+          supportDescription3: " З повагою, команда проєкту \"Українські колискові\".",
           ourPartners: "Наші партнери",
           feedbackForm: "Форма зворотнього звʼязку",
           contactUs: "Якщо у Вас є питання/пропозиції/побажання - чекаємо Ваших повідомлень.",
