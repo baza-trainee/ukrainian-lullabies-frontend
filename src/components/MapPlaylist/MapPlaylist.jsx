d59e940f0e38fb94d0d5a7fdffd056d221ba2a7a
Diana Tuz <dianatuz12@gmail.com>
2023-10-04 23:32:54 +0300

    fix classname

--- src/components/MapPlaylist/MapPlaylist.jsx
+++ src/components/MapPlaylist/MapPlaylist.jsx
@@ -39,7 +39,7 @@ export const MapPlaylist = () => {
   }, [])
 
   return (
-    <section id="anima" className="playlist margin-bottom">
+    <section id="anima" className="map-playlist margin-bottom">
       <div className="playlist-map">
         <div >
           <img className="map-playlist-png" src={ MapSvg } alt="map" />
