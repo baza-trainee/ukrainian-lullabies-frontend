74a12bb5a32b96379c50f81c36e20b45cc5362c8
IrynaShashko <95182459+IrynaShashko@users.noreply.github.com>
2023-10-05 12:44:24 +0300

    feat: fix achivements

--- src/components/OurAchivements/Donat/donat.jsx
+++ src/components/OurAchivements/Donat/donat.jsx
@@ -4,13 +4,14 @@ import { PatreonLogo } from "../PatreonLogo/PatreonLogo";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import "./donat.css";
+import { Ornaments } from "../../Ornaments/Ornaments";
 
 export const Donat = () => {
   const { t } = useTranslation();
   return (
     <>
       <div className="achievements-support-container">
-        <p className="text-2xl">{ t("helpWith") }</p>
+        <p className="text-2xl">{t("helpWith")}</p>
         <div className="donats">
           <Link href="#">
             <CoffeeLogo />
@@ -19,6 +20,9 @@ export const Donat = () => {
             <PatreonLogo />
           </Link>
         </div>
+      </div>
+      <div className="donats-ornament">
+        <Ornaments />
       </div>
     </>
   )
