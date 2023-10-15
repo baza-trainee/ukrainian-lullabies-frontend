a3e310cbf9d75a74a1c12c487f955ad3468429b9
BodNS <bondarenokn@gmail.com>
2023-10-15 19:31:59 +0200

    added chatBot Technical Works

--- src/components/Chatbot/Chatbot.jsx
+++ src/components/Chatbot/Chatbot.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
-import { useInView } from 'react-intersection-observer';
+import { useInView } from "react-intersection-observer";
 import { useSelector } from "react-redux";
 import classNames from "classnames";
+
+import { ChatbotTechnicalWork } from "./ChatbotTechnicalWork";
+
 import "./Chatbot.css";
+import "../TechnicalWorks/TechnicalWorks.css";
 
 const Chatbot = () => {
   const isLightTheme = useSelector((state) => state.theme.isLightTheme);
@@ -25,54 +29,58 @@ const Chatbot = () => {
   const { ref, inView } = useInView({
     triggerOnce: true,
   });
-  
 
   return (
     <motion.div
       initial="hidden"
-      animate={ inView ? "visible" : "hidden" }
-      variants={ animationElement }
-      ref={ ref }
+      animate={inView ? "visible" : "hidden"}
+      variants={animationElement}
+      ref={ref}
       className="container chat_wrapper"
     >
       <div className="chat_info">
-        <motion.p custom={ 1 } variants={ animationElement } className="text-3xl">
-          { t("chatBot") }
+        <motion.p custom={1} variants={animationElement} className="text-3xl">
+          {t("chatBot")}
         </motion.p>
         <motion.ul
-          custom={ 2 }
-          variants={ animationElement }
+          custom={2}
+          variants={animationElement}
           className="chat_rule_list"
         >
-          <li className="text-base chat_text">{ t("playEducationalGameWithChatbot") }</li>
-          <li className="text-base chat_text">{ t("youCanGuessWhichRegion") }</li>
-          <li className="text-base chat_text">{ t("youCanLearnAboutThePlots") }</li>
-          <li className="text-base chat_text">{ t("letsPlay") }</li>
+          <li className="text-base chat_text">
+            {t("playEducationalGameWithChatbot")}
+          </li>
+          <li className="text-base chat_text">{t("youCanGuessWhichRegion")}</li>
+          <li className="text-base chat_text">
+            {t("youCanLearnAboutThePlots")}
+          </li>
+          <li className="text-base chat_text">{t("letsPlay")}</li>
         </motion.ul>
       </div>
-      <motion.div custom={ 3 } variants={ animationElement } className="button chat_button">
+      <ChatbotTechnicalWork isLightTheme={isLightTheme} />
+      {/* <motion.div custom={ 3 } variants={ animationElement } className="button chat_button">
         <Link href="#" className="text-base-semibold">
           { t("play") }
         </Link>
-      </motion.div>
+      </motion.div> */}
       <motion.div
-        custom={ 3 }
-        variants={ animationElement }
+        custom={3}
+        variants={animationElement}
         className="chat_picture"
       >
         <Link
           href="#"
-          className={ classNames("chat_image_dark_ua", {
+          className={classNames("chat_image_dark_ua", {
             chat_image_white_ua: isLightTheme,
             ua: !isEnglishLanguage,
-          }) }
+          })}
         ></Link>
         <Link
           href="#"
-          className={ classNames("chat_image_dark_en", {
+          className={classNames("chat_image_dark_en", {
             chat_image_white_en: isLightTheme,
             en: isEnglishLanguage,
-          }) }
+          })}
         ></Link>
       </motion.div>
     </motion.div>
