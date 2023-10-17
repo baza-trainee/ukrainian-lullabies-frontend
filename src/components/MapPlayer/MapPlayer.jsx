fe6f792d4493ad7162c43febbff6696c1e8add80
Diana Tuz <124447185+diana-tuz@users.noreply.github.com>
2023-10-17 21:40:19 +0300

    Bugfixing/animation lullabies (#207)
    * fix map
    
    * yep
    
    * add some fix
    
    * fix styles add prefixes

--- src/components/MapPlayer/MapPlayer.jsx
+++ src/components/MapPlayer/MapPlayer.jsx
@@ -24,6 +24,7 @@ const songsData = [
     name: "Колискова для мами",
     lyrics: 'колискова для мами слова',
     duration: '00:59',
+    region: 'Полісся',
   },
   {
     id: 1,
@@ -31,6 +32,7 @@ const songsData = [
     name: "Ходить сон бiля вiкон",
     lyrics: "Ой ходить сон коло вікон, \n А дрімота — коло плота.\nПитається сон дрімоти:\n — А де будем ночувати? \n \n\n \nДе хатонька теплесенька,\nДе дитина малесенька,—Там ми будем ночувати,Дитиночку колихати.\nОй на кота та воркота,На дитину та й дрімота,Котик буде воркотати,\n\n  Дитинонька буде спати. Ой ходить сон коло вікон, \n А дрімота — коло плота.\nПитається сон дрімоти:\n — А де будем ночувати? \n \n\n \nДе хатонька теплесенька,\nДе дитина малесенька,—Там ми будем ночувати,Дитиночку колихати.\nОй на кота та воркота,На дитину та й дрімота,Котик буде воркотати,\n\n  Дитинонька буде спати.",
     duration: '00:59',
+    region: 'Карпати',
   },
   {
     id: 2,
@@ -38,6 +40,7 @@ const songsData = [
     name: "Котику сіренький",
     lyrics: 'Котику Сіренький \n Котику Біленький \n Котку Волохатий \nне ходи по Хаті \n Не ходи по Хаті \n не буди Дитяти \n Дитя буде спати \n Котик воркотати \nОй на Кота на Воркота \nНа Дитинку Дрімота \n(А-а а-а а-а а)',
     duration: '00:59',
+    region: 'Полісся',
   },
   {
     id: 3,
@@ -45,13 +48,15 @@ const songsData = [
     name: "Колискова",
     lyrics: 'Котику сіренький текст',
     duration: '00:59',
+    region: 'Карпати',
   },
   {
     id: 4,
     url: "https://soundbible.com/mp3/Radio%20Tune-SoundBible.com-1525681700.mp3",
     name: "Radio tune",
     watches: 2000,
-    duration: "0:05",
+    duration: "00:05",
+    region: 'Полісся',
   },
 ];
 
@@ -256,6 +261,7 @@ export const MapPlayer = () => {
           <h3 className="current-name text-l">
             { currentName }
           </h3>
+          <p className="region text-base">{ data[currentSongIndex].region }</p>
           <div className={ classNames('progress-bar', {
             'progress-bar-light': isLightTheme,
             'progress-bar-dark': !isLightTheme,
