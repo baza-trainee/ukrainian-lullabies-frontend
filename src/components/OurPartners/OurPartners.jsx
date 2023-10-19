908a3981dcd7590d229d5e234f51310404c54b94
Diana Tuz <dianatuz12@gmail.com>
2023-10-19 21:02:13 +0300

    fix partners from backend

--- src/components/OurPartners/OurPartners.jsx
+++ src/components/OurPartners/OurPartners.jsx
@@ -1,22 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 import { useTranslation } from 'react-i18next';
 
 import { OrnamentDouble, OrnamentTriple } from '../../icons/OrnamentTripple';
 import './OurPartners.css';
 import { motion } from "framer-motion";
 import { useInView } from 'react-intersection-observer';
-import BazaDark from '../../images/Card_partners_black_baza.png';
-import BazaLight from '../../images/Card_partners_white_baza.png';
-import EtnoDark from '../../images/Card_partners_black_etno.png';
-import EtnoLight from '../../images/Card_partners_white-etno.png';
-import Red from '../../images/Card_partners_red.png';
+import { fetchPartners } from '../../redux/Partners/partnersSlice';
 
 export const OurPartners = () => {
   const isLightTheme = useSelector((state) => state.theme.isLightTheme);
-  const bazaSvg = isLightTheme ? BazaLight : BazaDark;
-  const etnoSvg = isLightTheme ? EtnoLight : EtnoDark;
   const { ref, inView } = useInView({
     triggerOnce: true,
   });
@@ -32,27 +26,53 @@ export const OurPartners = () => {
     }),
   };
   const { t } = useTranslation();
+  const partners = useSelector((state) => state.partners.data);
+  const partnersError = useSelector((state) => state.partners.error);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchPartners());
+  }, [dispatch]);
+
   return (
     <motion.section
       initial="hidden"
       animate={ inView ? "visible" : "hidden" }
       variants={ animationElement }
       ref={ ref }
-      className='margin-bottom'>
+      className='partners margin-bottom'>
       <h2 className='text-4xl ourPartners'>{ t('ourPartners') }</h2>
       <motion.div
         custom={ 1 }
         variants={ animationElement }
         className='partners-container margin-bottom'>
-        <Link to="https://baza-trainee.tech">
-          <img src={ bazaSvg } alt='Baza' />
-        </Link>
-        <Link to="https://www.facebook.com/etnofotka/photos/">
-          <img src={ etnoSvg } alt='Etno' />
-        </Link>
-        <Link to="#">
-          <img src={ Red } alt='Partners' />
-        </Link>
+        { !partnersError ? (
+          partners.map((partner, index) => {
+            return (
+              <><Link
+                to={ partner.website }
+                target="_blank"
+                rel="noopener nofollow noreferrer"
+                title={ partner.name }
+                key={ index }
+
+              >
+                <img
+                  src={ isLightTheme ? partner.classic_logo : partner.dark_logo }
+                  alt={ partner.name } className='partners-icons' />
+              </Link><Link
+                to={ partner.website }
+                target="_blank"
+                rel="noopener nofollow noreferrer"
+                title={ partner.name }
+                key={ index }
+
+              >
+                  <img
+                    src={ isLightTheme ? partner.classic_logo : partner.dark_logo }
+                    alt={ partner.name } className='partners-icons' />
+                </Link></>
+            )
+          })) : <div>Error: { partnersError }</div> }
       </motion.div>
       <motion.div custom={ 2 }
         variants={ animationElement } className='ornament-triple margin-bottom'>
