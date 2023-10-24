bae391777912de68f6e6f1f8c00a5c24c7330401
BodNS <71136538+BodNS@users.noreply.github.com>
2023-10-24 22:48:03 +0200

    add img-optimization in AboutUs support (#240)

--- src/components/AboutUsInfo/AboutUsInfo.jsx
+++ src/components/AboutUsInfo/AboutUsInfo.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import Patreon from "./patreon";
 import { motion } from "framer-motion";
-import { useInView } from 'react-intersection-observer';
-import { GeneralTitle } from '../GeneralTitle/GeneralTitle';
-import { Ornaments } from '../Ornaments/Ornaments';
+import { useInView } from "react-intersection-observer";
+import { GeneralTitle } from "../GeneralTitle/GeneralTitle";
+import { Ornaments } from "../Ornaments/Ornaments";
 import "./AboutUsInfo.css";
 
 const AboutUsInfo = () => {
@@ -31,80 +31,116 @@ const AboutUsInfo = () => {
       animate={inView ? "visible" : "hidden"}
       variants={animationElement}
       ref={ref}
-      className='container wrapper margin-bottom'
+      className="container wrapper margin-bottom"
     >
-      <motion.div custom={1} variants={animationElement} className='technikal__container container'>
+      <motion.div
+        custom={1}
+        variants={animationElement}
+        className="technikal__container container"
+      >
         <GeneralTitle />
       </motion.div>
 
       <motion.p
         custom={1}
         variants={animationElement}
-        className='text-4xl title title_margin-bottom'
+        className="text-4xl title title_margin-bottom"
       >
-        {t('aboutUs')}
+        {t("aboutUs")}
       </motion.p>
       <motion.div
         initial="hidden"
         whileInView="visible"
         custom={1}
         variants={animationElement}
-        className='target-container'>
-        <div className='img-container target-img'>
-          <img src='/img/aboutUs/aboutUs-target.png' alt='mother with baby' />
+        className="target-container"
+      >
+        <div className="img-container target-img">
+          <img
+            src="/img/aboutUs/aboutUs-target.png"
+            alt="mother with baby"
+            loading="lazy"
+          />
         </div>
-        <div className='target-info'>
-          <p className='text-2xl title-small'>
-            {t('projectPurpose')}
-          </p>
-          <p className='text-base'>
-            {t('projectDescription')}
-          </p>
-          <p className='text-base'>
-            {t('projectDescription2')}
-          </p>
+        <div className="target-info">
+          <p className="text-2xl title-small">{t("projectPurpose")}</p>
+          <p className="text-base">{t("projectDescription")}</p>
+          <p className="text-base">{t("projectDescription2")}</p>
         </div>
       </motion.div>
-      <div className='ornaments-about'>
+      <div className="ornaments-about">
         <Ornaments />
       </div>
-      <motion.div initial="hidden"
+      <motion.div
+        initial="hidden"
         whileInView="visible"
         custom={2}
         variants={animationElement}
-        className='support-container'
+        className="support-container"
       >
-        <div className='support-info'>
-          <p className='text-2xl title-small'>
-            {t('supportUs')}
-          </p>
-          <p className='text-base'>
-            {t('supportDescription')}
-          </p>
-          <p className='text-base'>
-            {t('supportDescription2')}
-          </p>
-          <p className='text-base'>
-            {t('supportDescription3')}
-          </p>
-          <p className='text-base-semibold'>
-            {t('donateHere')}
-          </p>
+        <div className="support-info">
+          <p className="text-2xl title-small">{t("supportUs")}</p>
+          <p className="text-base">{t("supportDescription")}</p>
+          <p className="text-base">{t("supportDescription2")}</p>
+          <p className="text-base">{t("supportDescription3")}</p>
+          <p className="text-base-semibold">{t("donateHere")}</p>
         </div>
-        <div className='donat-imgs'>
-          <div className='donat-bmc donat_margin-right'>
-            <Link to='https://www.buymeacoffee.com/kolyskova' className="buy-me-a-caffee" target="_blank"></Link>
+        <div className="donat-imgs">
+          <div className="donat-bmc donat_margin-right">
+            <Link
+              to="https://www.buymeacoffee.com/kolyskova"
+              className="buy-me-a-caffee"
+              target="_blank"
+            ></Link>
           </div>
-          <div className='donat-patreon'>
-            <Link to='https://www.patreon.com/KolyskovaMuseum' target="_blank"><Patreon /></Link>
+          <div className="donat-patreon">
+            <Link to="https://www.patreon.com/KolyskovaMuseum" target="_blank">
+              <Patreon />
+            </Link>
           </div>
         </div>
-        <div className='img-container support-img'>
-          <img src='/img/aboutUs/aboutUs-support.png' alt='mother with baby' />
+        <div className="img-container support-img">
+          <picture>
+            <source
+              srcSet="/img/aboutUs/aboutUs-support-desk-w.webp"
+              media="(min-width: 1440px)"
+              type="image/webp"
+            />
+            <source
+              srcSet="/img/aboutUs/aboutUs-support-desk-1x.jpg 1x, /img/aboutUs/aboutUs-support-desk-2x.jpg 2x"
+              media="(min-width: 1440px)"
+              type="image/jpg"
+            />
+            <source
+              srcSet="/img/aboutUs/aboutUs-support-tab-w.webp"
+              media="(min-width: 768px)"
+              type="image/webp"
+            />
+            <source
+              srcSet="/img/aboutUs/aboutUs-support-tab-1x.jpg 1x, /img/aboutUs/aboutUs-support-tab-2x.jpg 2x"
+              media="(min-width: 768px)"
+              type="image/jpg"
+            />
+            <source
+              srcSet="/img/aboutUs/aboutUs-support-mob-w.webp"
+              media="(min-width: 360px)"
+              type="image/webp"
+            />
+            <source
+              srcSet="/img/aboutUs/aboutUs-support-mob-1x.jpg 1x, /img/aboutUs/aboutUs-support-mob-2x.jpg 2x"
+              media="(min-width: 360px)"
+              type="image/jpg"
+            />
+            <img
+              src="/img/aboutUs/aboutUs-support.png"
+              alt="mother with baby"
+              loading="lazy"
+            />
+          </picture>
         </div>
       </motion.div>
     </motion.section>
-  )
-}
+  );
+};
 
 export default AboutUsInfo;
