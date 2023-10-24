fed48d526cff008576f277ba3a1615341caf1614
IrynaShashko <95182459+IrynaShashko@users.noreply.github.com>
2023-10-24 16:15:11 +0300

    fix

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,77 +1,79 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { motion } from "framer-motion";
 
-import './loader.css';
+import "./loader.css";
 
 export const Loader = () => {
-    const [lines, setLines] = useState([1, 2, 3, 4, 5, 6]);
-    const [activeLineIndex, setActiveLineIndex] = useState(0);
-    const isLightTheme = useSelector((state) => state.theme.isLightTheme);
-    const styleDiv = isLightTheme ? "loader-line" : "loader-line_black";
+  const [lines, setLines] = useState([1, 2, 3, 4, 5, 6]);
+  const [activeLineIndex, setActiveLineIndex] = useState(0);
+  const isLightTheme = useSelector((state) => state.theme.isLightTheme);
+  const styleDiv = isLightTheme ? "loader-line" : "loader-line_black";
 
-    const animationElement = {
-        hidden: {
-            y: -50,
-            opacity: 0,
-        },
-        visible: (custom) => ({
-            y: 0,
-            opacity: 1,
-            transition: { ease: "easeOut", duration: 2, delay: custom * 0.3 },
-        }),
-    };
+  const animationElement = {
+    hidden: {
+      y: -50,
+      opacity: 0,
+    },
+    visible: (custom) => ({
+      y: 0,
+      opacity: 1,
+      transition: { ease: "easeOut", duration: 2, delay: custom * 0.3 },
+    }),
+  };
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            const newLines = lines.map(() => {
-                const lineStyles = {
-                    width: "8px",
-                    height: "44px",
-                    borderRadius: "10px",
-                    background: "#000",
-                };
-
-                if (window.innerWidth >= 1440) {
-                    lineStyles.width = "16px";
-                    lineStyles.height = "54px";
-                }
-
-                return lineStyles;
-            });
-
-            newLines.forEach((line, index) => {
-                if (index === activeLineIndex) {
-                    line.className = 'loader-line_active';
-                } else {
-                    line.className = styleDiv;
-                }
-            });
-
-            setActiveLineIndex((prevIndex) =>
-                prevIndex < lines.length - 1 ? prevIndex + 1 : 0
-            );
-
-            setLines(newLines);
-        }, 500);
-
-        return () => {
-            clearInterval(interval);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newLines = lines.map(() => {
+        const lineStyles = {
+          width: "8px",
+          height: "44px",
+          borderRadius: "10px",
+          background: "#000",
         };
-    }, [lines, activeLineIndex]);
 
-    return (
-        <motion.div custom={1}
-            variants={animationElement} className="loader-container">
-            <div className="loader">
-                {lines.map((line, index) => (
-                    <div
-                        className={`${styleDiv} ${line.className || ''}`}
-                        key={index}
-                    ></div>
-                ))}
-            </div>
-        </motion.div>
-    );
-}
+        if (window.innerWidth >= 1440) {
+          lineStyles.width = "16px";
+          lineStyles.height = "54px";
+        }
 
+        return lineStyles;
+      });
+
+      newLines.forEach((line, index) => {
+        if (index === activeLineIndex) {
+          line.className = "loader-line_active";
+        } else {
+          line.className = styleDiv;
+        }
+      });
+
+      setActiveLineIndex((prevIndex) =>
+        prevIndex < lines.length - 1 ? prevIndex + 1 : 0
+      );
+
+      setLines(newLines);
+    }, 500);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [lines, activeLineIndex]);
+
+  return (
+    <motion.div
+      custom={1}
+      variants={animationElement}
+      className="loader-container"
+    >
+      <div className="loader">
+        {lines.map((line, index) => (
+          <div
+            className={`${styleDiv} ${line.className || ""}`}
+            key={index}
+          ></div>
+        ))}
+      </div>
+    </motion.div>
+  );
+};
--- src/components/TechnicalWorks/TechnicalWorks.jsx
+++ src/components/TechnicalWorks/TechnicalWorks.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 import classNames from "classnames";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { motion } from "framer-motion";
-import { useInView } from 'react-intersection-observer';
-import { useTranslation } from 'react-i18next';
+import { useInView } from "react-intersection-observer";
+import { useTranslation } from "react-i18next";
 
 import OrnamentsLeftIcon from "../../icons/OrnamentsLeftIcon";
 import OrnamentsRightIcon from "../../icons/OrnamentsRightIcon";
-import { GeneralTitle } from '../GeneralTitle/GeneralTitle';
-import { Loader } from '../Loader/Loader';
-import './TechnicalWorks.css'
+import { GeneralTitle } from "../GeneralTitle/GeneralTitle";
+import { Loader } from "../Loader/Loader";
+import "./TechnicalWorks.css";
 
 export const TechnicalWorks = () => {
   const isLightTheme = useSelector((state) => state.theme.isLightTheme);
@@ -36,14 +36,13 @@ export const TechnicalWorks = () => {
   });
   const { t } = useTranslation();
   return (
-
     <motion.section
       initial="hidden"
       animate={inView ? "visible" : "hidden"}
       variants={animationElement}
       custom={1}
       ref={ref}
-      className='technikal'
+      className="technikal"
     >
       <div className="technical_content">
         <div className="technikal__border ">
@@ -51,48 +50,50 @@ export const TechnicalWorks = () => {
             <GeneralTitle />
           </div>
         </div>
-
-        <motion.div custom={3}
+        <motion.div
+          custom={3}
           variants={animationElement}
-          className="technikal__block container">
-          <div className='tech-wrap'>
-            <div className='loader'>
+          className="technikal__block container"
+        >
+          <div className="tech-wrap">
+            <div className="loader">
               <Loader />
             </div>
-
             <div className="tech-container">
               <div className="right">
                 <OrnamentsLeftIcon />
               </div>
               <div>
                 <p className="center text-base">
-
-                  {t('technikalWorsk')}
-
-                  {t('technikalWorsk2')}
-
+                  {t("technikalWorsk")}
+                  {t("technikalWorsk2")}
                 </p>
               </div>
-
               <div className="left">
                 <OrnamentsRightIcon />
               </div>
             </div>
           </div>
         </motion.div>
-        <motion.div custom={4}
-          variants={animationElement} className="container">
+        <motion.div
+          custom={4}
+          variants={animationElement}
+          className="container"
+        >
           <NavLink
             to="/"
-            className={classNames('button', 'technikal-button', { 'button-dark': !isLightTheme, })}
+            className={classNames("button", "technikal-button", {
+              "button-dark": !isLightTheme,
+            })}
             onClick={() => {
               scrollToTarget("#header");
-            }}> {t('toTheMain')} </NavLink>
+            }}
+          >
+            {" "}
+            {t("toTheMain")}{" "}
+          </NavLink>
         </motion.div>
       </div>
-
     </motion.section>
-
-
-  )
+  );
 };
