46504381272b68aba40517c30769466d2d3a6ce9
shologin <136815194+shologin@users.noreply.github.com>
2023-11-22 20:52:14 +0200

    Feat/footer/docs (#299)
    * feat: added links to docs in footer and getting docs via API
    
    * feat: added fetching achievements via API and displaying in OurAchievements
    
    ---------
    
    Co-authored-by: shologin <mockingbird@mail.com>

---
+++ src/redux/achievements/achievementsSlice.js
@@ -0,0 +1,39 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  loading: false,
+  data: {},
+  error: "",
+};
+
+export const fetchAchievements = createAsyncThunk("achievements/fetchAchievements", async () => {
+  try {
+    const response = await axios.get("https://api.kolyskova.com/statistic/");
+    const data = response.data;
+    return data;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+});
+
+const achievementsSlice = createSlice({
+  name: "achievements",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchAchievements.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchAchievements.fulfilled, (state, action) => {
+      state.loading = false;
+      state.data = action.payload;
+    });
+    builder.addCase(fetchAchievements.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.error.message;
+    });
+  },
+});
+
+export default achievementsSlice.reducer;
