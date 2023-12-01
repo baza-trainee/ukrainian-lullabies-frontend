ebefcd0f9cc0c9514e45e3b9610fe21874253534
shologin <136815194+shologin@users.noreply.github.com>
2023-12-01 21:49:36 +0200

    feat: added close button to qr-modal (#305)
    Co-authored-by: shologin <mockingbird@mail.com>

--- src/components/OurAchivements/QRCode/QrCodeButton.jsx
+++ src/components/OurAchivements/QRCode/QrCodeButton.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 import Popup from "reactjs-popup";
 import ornamentImg from "../../../assets/images/OrnamentsMapTabs.svg";
+import { FiX } from "react-icons/fi";
 import QRCode from "../../../assets/images/QR_Code.svg";
 import { QrCodeIcon } from "../QRCode/QrCodeIcon";
 import "./QrCodeButton.css";
@@ -41,11 +42,7 @@ export const QrCodeButton = ({ mobile = false }) => {
       }
       modal
       lockScroll
-      overlayStyle={
-        isLightTheme
-          ? { background: "rgba(0, 0, 0, 0.6)" }
-          : { background: "rgba(231, 231, 231, 0.6)" }
-      }
+      overlayStyle={isLightTheme ? { background: "rgba(0, 0, 0, 0.6)" } : { background: "rgba(231, 231, 231, 0.6)" }}
     >
       {(close) => (
         <div
@@ -53,6 +50,17 @@ export const QrCodeButton = ({ mobile = false }) => {
             "qr-button-modal-light": isLightTheme,
           })}
         >
+          <div className="qr-closeBtn">
+            <FiX
+              style={{
+                width: "48px",
+                height: "48px",
+                padding: "12px",
+                cursor: "pointer",
+              }}
+              onClick={close}
+            />
+          </div>
           <div className="qr-image">
             <img src={QRCode} alt="QR" width="140" height="140" />
           </div>
@@ -60,140 +68,78 @@ export const QrCodeButton = ({ mobile = false }) => {
           <ul className="qr-info">
             <li>
               <p className="text-xs"> {t("recipientName")}:</p>
-              <p
-                className="text-base-semibold"
-                onClick={(e) => copyToClipboard(e, 0)}
-              >
-   {t("ngoLullabyMuseum")}
+              <p className="text-base-semibold" onClick={(e) => copyToClipboard(e, 0)}>
+                {t("ngoLullabyMuseum")}
               </p>
               <p
-                className={classNames(
-                  "qr-info-copy-clicked",
-                  "text-sm-semibold",
-                  {
-                    "qr-info-copy-clicked-light": isLightTheme,
-                  }
-                )}
-                style={
-                  copyClicked[0]
-                    ? { display: "inline-block" }
-                    : { display: "none" }
-                }
+                className={classNames("qr-info-copy-clicked", "text-sm-semibold", {
+                  "qr-info-copy-clicked-light": isLightTheme,
+                })}
+                style={copyClicked[0] ? { display: "inline-block" } : { display: "none" }}
               >
                 {t("shareText")}
               </p>
             </li>
             <li>
               <p className="text-xs"> {t("recipientCode")}:</p>
-              <p
-                className="text-base-semibold"
-                onClick={(e) => copyToClipboard(e, 1)}
-              >
+              <p className="text-base-semibold" onClick={(e) => copyToClipboard(e, 1)}>
                 45284215
               </p>
               <p
-                className={classNames(
-                  "qr-info-copy-clicked",
-                  "text-sm-semibold",
-                  {
-                    "qr-info-copy-clicked-light": isLightTheme,
-                  }
-                )}
-                style={
-                  copyClicked[1]
-                    ? { display: "inline-block" }
-                    : { display: "none" }
-                }
+                className={classNames("qr-info-copy-clicked", "text-sm-semibold", {
+                  "qr-info-copy-clicked-light": isLightTheme,
+                })}
+                style={copyClicked[1] ? { display: "inline-block" } : { display: "none" }}
               >
                 {t("shareText")}
               </p>
             </li>
             <li>
               <p className="text-xs">{t("recipientAccount")}:</p>
-              <p
-                className="text-base-semibold"
-                onClick={(e) => copyToClipboard(e, 2)}
-              >
+              <p className="text-base-semibold" onClick={(e) => copyToClipboard(e, 2)}>
                 UA353052990000026006035028980
               </p>
               <p
-                className={classNames(
-                  "qr-info-copy-clicked",
-                  "text-sm-semibold",
-                  {
-                    "qr-info-copy-clicked-light": isLightTheme,
-                  }
-                )}
-                style={
-                  copyClicked[2]
-                    ? { display: "inline-block" }
-                    : { display: "none" }
-                }
+                className={classNames("qr-info-copy-clicked", "text-sm-semibold", {
+                  "qr-info-copy-clicked-light": isLightTheme,
+                })}
+                style={copyClicked[2] ? { display: "inline-block" } : { display: "none" }}
               >
                 {t("shareText")}
               </p>
             </li>
             <li>
               <p className="text-xs">{t("bankName")}:</p>
-              <p
-                className="text-base-semibold"
-                onClick={(e) => copyToClipboard(e, 3)}
-              >
-
+              <p className="text-base-semibold" onClick={(e) => copyToClipboard(e, 3)}>
                 {t("jscCbPrivatbank")}
               </p>
               <p
-                className={classNames(
-                  "qr-info-copy-clicked",
-                  "text-sm-semibold",
-                  {
-                    "qr-info-copy-clicked-light": isLightTheme,
-                  }
-                )}
-                style={
-                  copyClicked[3]
-                    ? { display: "inline-block" }
-                    : { display: "none" }
-                }
+                className={classNames("qr-info-copy-clicked", "text-sm-semibold", {
+                  "qr-info-copy-clicked-light": isLightTheme,
+                })}
+                style={copyClicked[3] ? { display: "inline-block" } : { display: "none" }}
               >
                 {t("shareText")}
               </p>
             </li>
             <li>
               <p className="text-xs">{t("paymentPurpose")}:</p>
-              <p
-                className="text-base-semibold"
-                onClick={(e) => copyToClipboard(e, 4)}
-              >
+              <p className="text-base-semibold" onClick={(e) => copyToClipboard(e, 4)}>
                 {t("paymentPurposePlaceholder1")}
               </p>
-              <p className="text-base-semibold">
-                {t("paymentPurposePlaceholder2")}
-              </p>
+              <p className="text-base-semibold">{t("paymentPurposePlaceholder2")}</p>
 
               <p
-                className={classNames(
-                  "qr-info-copy-clicked",
-                  "text-sm-semibold",
-                  {
-                    "qr-info-copy-clicked-light": isLightTheme,
-                  }
-                )}
-                style={
-                  copyClicked[4]
-                    ? { display: "inline-block" }
-                    : { display: "none" }
-                }
+                className={classNames("qr-info-copy-clicked", "text-sm-semibold", {
+                  "qr-info-copy-clicked-light": isLightTheme,
+                })}
+                style={copyClicked[4] ? { display: "inline-block" } : { display: "none" }}
               >
                 {t("shareText")}
               </p>
             </li>
           </ul>
-          <img
-            src={ornamentImg}
-            alt="ornament"
-            className="qr-button-modal-ornament"
-          />
+          <img src={ornamentImg} alt="ornament" className="qr-button-modal-ornament" />
         </div>
       )}
     </Popup>
