17f31622961d76b6303b2bdfd4c4fa6c6e032d3c
Diana Tuz <dianatuz12@gmail.com>
2024-04-04 18:42:56 +0300

    change h1

--- src/components/GeneralTitle/GeneralTitle.jsx
+++ src/components/GeneralTitle/GeneralTitle.jsx
@@ -1,25 +1,16 @@
 import React from "react";
 import "../../components/GeneralTitle/general-title.css";
 import SIcon from "../../icons/SIcon";
-import KolyIcon from "../../icons/KolyIcon";
-import KovaIcon from "../../icons/KovaIcon";
-import OundIcon from "../../icons/OundIcon";
 
 export const GeneralTitle = () => {
   return (
-    <div className="kolyskova-container">
-      <div className="kolyIcon text-5xl">
-        <KolyIcon />
-      </div>
+    <div className="kolyskova-container" translate="no">
+      <div className="kolyIcon text-5xl">Koly</div>
       <div className="letterS">
         <SIcon />
       </div>
-      <div className="kovaIcon text-5xl">
-        <KovaIcon />
-      </div>
-      <div className="oundIconWrap text-5xl">
-        <OundIcon />
-      </div>
+      <div className="kovaIcon text-5xl">Kova</div>
+      <div className="oundIconWrap text-5xl">ound</div>
     </div>
   );
 };
